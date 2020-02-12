a04b0118fedd920ef61617da4f7decb39d307d1d
Malcom Fetus <sora.vista@gmail.com>
2020-02-12 00:45:42 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,14 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def parrot
+  puts "Squawk!"
+  'Squawk!'
+  return "Squawk!"
+end
+
+def parrot(Pretty bird!)
+  puts "Squawk!"
+  'Squawk!'
+  return "Squawk!"
+end
