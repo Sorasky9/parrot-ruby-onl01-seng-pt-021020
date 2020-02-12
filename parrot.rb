f353a6b058d4be3b4fa93a449fa1ba0d6390e68a
Malcom Fetus <sora.vista@gmail.com>
2020-02-12 01:01:25 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,14 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
 
-def parrot
+def parrot(bird="Pretty bird")
   puts "Squawk!"
   'Squawk!'
   return "Squawk!"
 end
 
-def parrot(bird="Pretty bird")
-  puts "#{bird}!"
-  
-  return "#{bird}"
-end
